2f569f6b607666512d6c9468d4070ad57539f4d4
Yuta Kobayashi <67689007+ntaaaa@users.noreply.github.com>
2021-03-12 14:23:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 
 {
 
+  // メニューモーダル
   const open = document.getElementById('open');
   const menu = document.querySelector('.menu');
   const close = document.getElementById('close');
+
 
   open.addEventListener('click', ()=>{
     menu.classList.add('show');
@@ -12,11 +14,42 @@
     close.classList.add('show');
   });
 
+
   close.addEventListener('click', ()=>{
     menu.classList.remove('show');
     open.classList.remove('hide');
     close.classList.remove('show');
   });
+  menu.addEventListener('click', ()=>{
+    menu.classList.remove('show');
+    open.classList.remove('hide');
+    close.classList.remove('show');
+  });
+
+
+
+
+
+
+
+  // トップのフェードイン
+  $('.top h1').fadeIn(2500)
+
+
+  // イメージモーダル
+
+  $('.simg').click(function() {
+    const imgSrc = $(this).attr('src');
+    $('.bigimg').children().attr('src', imgSrc);
+    $('.img-modal').fadeIn();
+    $('body, html').css('overflow-y', 'hidden');
+  });
+
+  $('#img-close').click(function(){
+    $('.img-modal').fadeOut();
+    $('body, html').css('overflow-y', 'visible')
+  })
+ 
 
 
 }
